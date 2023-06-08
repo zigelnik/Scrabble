e2cbbaf225f2fa285b58fd938f69438997b817f3
lovto <lovton13@gmail.com>
2023-06-08 17:41:48 +0300

    Binding the Score

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.beans.binding.Bindings;
 import model.Model;
 import model.network.GameServer;
 import view_model.ViewModel;
@@ -8,31 +9,33 @@ import java.util.Observer;
 
 public class View implements Observer {
     LandingPage landingPage;
-//    WaitingPage waitingPage;
-    GamePage gamePage;
-   public static ViewModel vm = ViewModel.getViewModel();
+    GamePage gamePage = GamePage.getGP();
+    ViewModel vm = ViewModel.getViewModel();
 
 
     public View() {
         this.landingPage  = new LandingPage();
-//        this.waitingPage = new WaitingPage();
-        this.gamePage = new GamePage();
+        GamePage.getGP();
     }
+
+
+
+    public void setViewModel() {
+        //The lambda expression is used to convert the integer value to a string using Integer.toString().
+        //The second argument vm.score specifies that the binding should be recomputed whenever the value of vm.score changes.
+        gamePage.scoreLabel.textProperty().bind(vm.score.asString());
+
+    }
+
 
     @Override
     public void update(Observable o, Object arg) {
 
     }
 
-
     //GETTES
-
-
     public LandingPage getLandingPage() {return landingPage;}
-//    public WaitingPage getWaitingPage() {return waitingPage;}
     public GamePage getGamePage() {return gamePage;}
-
-
     private  static class ViewHolder{ public static final View v = new View();}
     public static View getView() {return ViewHolder.v;}
 
